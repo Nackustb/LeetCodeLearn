fd1e44dbe4e0436644f97676d69a6dc25aa64916
Nackustb <2249314748@qq.com>
2025-10-09 19:21:23 +0800

    109

--- q217.py
+++ q217.py
@@ -1,16 +1,14 @@
 from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashtable = set()
+        hash_set = set()
         for num in nums:
-            if num in hashtable:
+            if num not in hash_set:
+                hash_set.add(num)
+            else:
                 return True
-            hashtable.add(num) 
         return False
-# 测试
-if __name__ == "__main__":
-    solution = Solution()
-    nums = [1,2,3,1]
-    print(solution.containsDuplicate(nums))  # 输出 True
-    nums = [1,2,3,4]
-    print(solution.containsDuplicate(nums))  # 输出 False
+# test
+s = Solution()
+print(s.containsDuplicate([1,2,3,1]))  # True
+print(s.containsDuplicate([1,2,3,4]))  # False

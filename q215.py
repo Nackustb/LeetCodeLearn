a0f2a5ec1a2d1b577f725edb2a67b8520ccb3a71
Nackustb <2249314748@qq.com>
2025-10-11 13:22:34 +0800

    workseet

--- q215.py
+++ q215.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import heapq
-from typing import List
-class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-        for n in nums:
-            heapq.heappush(heap, n)
-            if len(heap) > k:
-                heapq.heappop(heap)
-        return heap[0]
-
-# 测试
-if __name__ == "__main__":
-    sol = Solution()
-    print(sol.findKthLargest([3,2,1,5,6,4], 2))  # 输出: 5
-    print(sol.findKthLargest([3,2,3,1,2,4,5,5,6], 4))  # 输出: 4
-=======
 from typing import List
 import heapq
 class Solution:
@@ -29,4 +11,3 @@ class Solution:
 # test
 s = Solution()
 print(s.findKthLargest([3,2,1,5,6,4], 2))  # 5
->>>>>>> fd1e44dbe4e0436644f97676d69a6dc25aa64916
